23e01a0ec32f038d45613e2cbbe05b343f98f462
Juboskar <oskari.juvakka@gmail.com>
2022-09-29 13:28:39 +0300

    ratings view, model, controller, database, styling navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
+  get 'ratings', to: 'ratings#index'
   resources :beers
   resources :breweries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
