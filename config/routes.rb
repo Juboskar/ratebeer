1832c0b7e19b50ef43fbfce5f02ed8120def8e55
Juboskar <oskari.juvakka@gmail.com>
2022-10-10 15:52:29 +0300

    scaffold users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'ratings', to: 'ratings#index'
   get 'ratings/new', to:'ratings#new'
   post 'ratings', to: 'ratings#create'
+  get 'signup', to: 'users#new'
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resources :users
 end
