323b4aaf95458e3b03a526f2bb645b28bb1f7d27
Juboskar <oskari.juvakka@gmail.com>
2022-10-10 14:49:46 +0300

    key - value pairs in authentication

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -70,9 +70,11 @@ class BreweriesController < ApplicationController
     end
 
     def authenticate
+      admin_accounts = { "pekka" => "beer", "arto" => "foobar", "matti" => "ittam", "vilma" => "kangas" }
+
       authenticate_or_request_with_http_basic do |username, password|
-        raise "Wrong username or password" unless username == "admin" and password == "secret"
-        
+        raise "Wrong username or password" unless admin_accounts[username] == password
+
         return true
       end
     end
