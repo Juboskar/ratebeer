ff1d16accf8b1d5b77128a16255300e4e938f152
Juboskar <oskari.juvakka@gmail.com>
2022-10-10 14:11:55 +0300

    refactor calculating average ratings to its own module

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,8 @@
 class Beer < ApplicationRecord
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-
-  def average_rating
-    ratings.average(:score).round(1)
-  end
 
   def to_s
     "#{name} #{brewery.name}"
