355dbeec1c36d4c4d621faeafcd06739fa53806a
Juboskar <oskari.juvakka@gmail.com>
2022-10-27 17:00:52 +0300

    db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :ratings
+end
+
+class Rating < ApplicationRecord
+  belongs_to :beer
+  belongs_to :user
+
+  def to_s
+    "#{beer.name} #{score}"
+  end
 end
