51f8217758b9c417d0ef2e1234303708bf2c3d28
Juboskar <oskari.juvakka@gmail.com>
2022-11-09 18:18:40 +0200

    better validation for brewery founding year

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,6 +6,14 @@ class Brewery < ApplicationRecord
                                     less_than_or_equal_to: 2022,
                                     only_integer: true }
 
+  validate :year_cannot_be_in_the_future
+
+  def year_cannot_be_in_the_future
+    if year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
